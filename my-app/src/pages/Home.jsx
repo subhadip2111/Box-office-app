7542c14857b163956f17704bf54475fe007811f7
subhadipshee <subhadipshee20010521@gmail.com>
2023-04-26 21:11:31 +0530

    api-show and fetch success

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,29 +1,46 @@
+import {searchForShows}from "./../api/tvmaze"
 import { useState } from "react"
-
-
-
-
 const Home=()=>{
-  const [inputvalue,setinputValue]=  useState('')
+  
+  const [searchStr,setSearchStr]=  useState('')
+  const [apiData,setapiData]=useState(null)
+  const [apidataError,setapidataError]=useState(null)
+  const onSearchInputChange=(ev)=>{
+    setSearchStr(ev.target.value)
+  }
+
+  const onSearch=async(ev)=>{
+    ev.preventDefault()
+   try {
+    setapidataError(null)
+    const result=await searchForShows(searchStr)
+    setapiData(result)
+   } catch (error) {
+    setapidataError(error)
+   }
+  }
 
 
-
-  const onInputChange=(ev)=>{
-    console.log(ev.target.value);
-
-    setinputValue(ev.target.value)
+  const renderApiData=()=>{
+    if(apidataError){
+      return <div>Error occur :{apidataError.message}</div>
+    }
+    if(apiData){
+return  apiData.map(data=><div key={data.show.id}>{data.show.name}</div>
+    )}
+    return null
   }
     return (
         <div> 
+<form onSubmit={onSearch}>
+<input type="text" value={searchStr} onChange={onSearchInputChange}></input>
+<button type="submit">Search</button>
+</form>
 
-        <div>{inputvalue}</div>
-        <button type="button" onClick={()=>{
-            setinputValue("subhadip")
-        }}> updated value</button>
-          <input type="text" value={inputvalue} onChange={onInputChange}></input> 
-            
-        
-        </div>
+<div>
+ {renderApiData()}
+</div>
+       </div>
     )
 }
 export default Home
